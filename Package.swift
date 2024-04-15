2c7bae72c3ed2b1116f285ada3f2621fb7e7ff0c
Boris Kolozsi <boris.kolozsi@gmail.com>
2024-04-15 15:32:05 +0200

    Update Package.swift
    AuthLib v 1.4.0

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SygicAuth",
-            url: "https://public.repo.sygic.com/repository/maven-sygic-releases/com/sygic/lib/auth/sygicauth-ios/1.3.2/sygicauth-ios-1.3.2.zip",
-            checksum: "b977c56a0f6d59defedd9f0aefd74785b7ae2942797a8cc97ecc5d417fd8666a"
+            url: "https://public.repo.sygic.com/repository/maven-sygic-releases/com/sygic/lib/auth/sygicauth-ios/1.4.0/sygicauth-ios-1.4.0.zip",
+            checksum: "566550c1c3fde2f35bf83c5f06f9e70c36ba0ba118015385a7e6d87a108c7c49"
         )
     ]
 )
